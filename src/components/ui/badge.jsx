cb5830057021d6fef08f5bcc17b805600fb11e76
Paul <paul.bourgois@student-cs.fr>
2025-08-02 16:08:30 +0200

    html

--- src/components/ui/badge.jsx
+++
@@ -1,4 +0,0 @@
-// src/components/ui/badge.jsx
-export const Badge = ({ children }) => (
-  <span className="minimal-badge">{children}</span>
-)
--- src/components/ui/separator.jsx
+++
@@ -1,5 +0,0 @@
-// src/components/ui/separator.jsx
-
-export const Separator = () => (
-  <hr className="my-4 border-gray-300 dark:border-gray-700" />
-)
