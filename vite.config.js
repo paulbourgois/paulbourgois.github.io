197bc6e9aa3198340a46a9bc414c83a0d6e21685
Paul <paul.bourgois@student-cs.fr>
2025-08-03 11:43:06 +0200

    Modification de la base URL dans vite.config.js pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Base path for GitHub Pages user site (username.github.io)
+  base: '', // Empty base for GitHub Pages user site (username.github.io)
   build: {
     outDir: 'dist',
     minify: 'terser',
